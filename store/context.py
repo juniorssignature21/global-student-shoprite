30dfe5d6495d322819f9e30c6b20398d58d66e60
Junior's Signature <162041570+juniorssignature21@users.noreply.github.com>
2025-01-15 10:13:55 +0100

    Update context.py

--- store/context.py
+++ store/context.py
@@ -4,8 +4,13 @@ def default(request):
     try:
         cart_id = request.session['cart_id']
         total_cart_items = store_models.Cart.objects.filter(cart_id=cart_id)
-    except:
-        total_cart_items = 0
-    return{
-        "total_cart_items":len(total_cart_items)
+    except KeyError:
+        # Handle the case where 'cart_id' is not in session
+        total_cart_items = []
+    except store_models.Cart.DoesNotExist:
+        # Handle the case where no cart items are found
+        total_cart_items = []
+    
+    return {
+        "total_cart_items": len(total_cart_items)
     }
