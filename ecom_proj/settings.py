a5af03a4350c072805684202af02857b05c9898d
Junior's Signature <162041570+juniorssignature21@users.noreply.github.com>
2025-01-20 07:26:37 +0100

    Update settings.py

--- ecom_proj/settings.py
+++ ecom_proj/settings.py
@@ -36,8 +36,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-&)0dbnucsu)$f4pgxd0ombb4)b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-# ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
+ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
 CSRF_TRUSTED_ORIGINS = ['http://127.0.0.1', 'https://global-student-shoprite-production.up.railway.app/']
 SECURE_CROSS_ORIGIN_OPENER_POLICY = 'same-origin-allow-popups'
 
